0c8e6b480b3dcecd58d868e64c7fe60dcae668a6
Juan <juansw03@gmail.com>
2024-04-04 19:31:38 -0500

    feat: added middleware to succesfully get something after post requests

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,17 @@ import apiRouter from "./app/api.router";
 const app = e();
 const port = 3000;
 
+app.use(e.json());
 
 app.get('/', (req, res) => {
   res.send('Servidor express')
 })
-
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Nueva ruta')
 })
 
 apiRouter(app);
-console.log('hey');
-
 
 app.listen(port, () => {
   console.log(`Corriendo en http://localhost:${port}`);
