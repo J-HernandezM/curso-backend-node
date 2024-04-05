28249dac81d6898ca0f076eb2a78959d9d628aa1
Juan <juansw03@gmail.com>
2024-04-04 19:31:09 -0500

    feat: added /api/v1 to all routes

--- src/app/api.router.ts
+++ src/app/api.router.ts
@@ -1,13 +1,15 @@
-import { Express } from 'express'
+import e, { Express } from 'express'
 import productsRouter from './products/products.router'
 import usersRouter from './users/users.router'
 import categoriesRouter from './categories/categories.router'
 
 const apiRouter = (app: Express) => {
-  app.use('/products', productsRouter)
-  app.use('/users', usersRouter)
-  app.use('/categories', categoriesRouter)
-  console.log('here');
+  const router = e.Router();
+
+  router.use('/products', productsRouter)
+  router.use('/users', usersRouter)
+  router.use('/categories', categoriesRouter)
+  app.use('/api/v1', router)
 }
 
 export default apiRouter
