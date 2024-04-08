1203c9b71515aa842fb8d1cd433d5cda045d5d26
Juan <juansw03@gmail.com>
2024-04-08 13:46:21 -0500

    feat: cors options and middleware applied

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 /* eslint-disable no-console */
 import e from "express";
 import apiRouter from "./app/api.router";
+import cors, { CorsOptions } from 'cors'
+
 import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler'
 
 const app = e();
 const port = 3000;
+const whitelist = ['http://localhost:5500', `http://localhost:${port}`]
+const options: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!whitelist.includes(origin!)) { callback(new Error('CORS not allowed')) }
+    callback(null, true)
+  }
+}
 
 app.use(e.json());
+app.use(cors(options))
 
 app.get('/', (req, res) => {
   res.send('Servidor express')
