75d64ac5066bb28a3b4c302fbcd7e537b347f165
Juan <juansw03@gmail.com>
2024-04-23 20:07:15 -0500

    feat: migrated to mysql using the same orm

--- src/lib/sequelize.ts
+++ src/lib/sequelize.ts
@@ -4,10 +4,11 @@ import { setupModels } from "../db/models";
 
 const USER = encodeURIComponent(config.dbUser!);
 const PASSWORD = encodeURIComponent(config.dbPassword!);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+// Para mysql usar puerto 3, usamos root como usario, usamos la misma clave, el puerto debe ser 33061, de resto igual
+const URI = `mysql://root:${PASSWORD}@${config.dbHost}:${config.dbPort}1/${config.dbName}`
 
 export const sequelize = new Sequelize(URI, {
-  dialect: 'postgres'
+  dialect: 'mysql'
 })
 
 setupModels(sequelize)
