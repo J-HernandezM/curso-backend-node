67aae03f5c739d958070f77a068e5cc600c97b9e
Juan <juansw03@gmail.com>
2024-05-15 16:43:41 -0500

    build: url changed to environment variable at config.ts inside migrations

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,13 +1,9 @@
 require('ts-node/register')
 const { config } = require('../config/config.ts')
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
-
 module.exports = {
   development: {
-    url: URI,
+    url: config.dbUrl,
     dialect: 'postgres'
   },
   production: {
